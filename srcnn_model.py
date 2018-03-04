030f836a7c777f57aca53e2fd3c4213f8189de0f
basher666 <swastik.haldar@gmail.com>
2018-03-04 15:00:38 +0530

    init commit

---
+++ srcnn_model.py
@@ -0,0 +1,30 @@
+import torch
+import torchvision
+import torch.nn as nn
+import numpy as np
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torchvision.datasets as d_sets
+from torch.utils.data import DataLoader as d_loader
+import matplotlib.pyplot as plt
+from PIL import image
+
+
+class SRCNN(nn.Module):
+    def __init__(self):
+        super(SRCNN,self).__init__()
+        self.conv1 = nn.Conv2d(3,64,kernel_size=9,padding=4);
+        self.relu1 = nn.ReLU();
+        self.conv2 = nn.Conv2d(64,32,kernel_size=1,padding=0);
+        self.relu2 = nn.ReLU();
+        self.conv3 = nn.Conv2d(32,3,kernel_size=5,padding=2);
+        
+    def forward(self,x):
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.conv3(out)
+
+        return out
+
